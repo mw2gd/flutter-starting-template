628821507f50c55e68878820de79fca54d90e927
Matthew Walters <mw2gd@virginia.edu>
2024-01-17 20:06:59 -0500

    Working: Good Starting Point

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,119 @@
-import 'package:flutter/material.dart';
+/// FlutterBluePrint App
+/// By Matt Walters
+///!!Make sure to cite Open Source libraries!!
 
+import 'package:flutter/material.dart';
+import 'package:flutterblueprint/util/scaffold_with_navbar.dart';
+import 'package:go_router/go_router.dart';
+
+///
+/// Entry Point of Application.
+///
 void main() {
-  runApp(const MainApp());
+  runApp(Root());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+///
+/// Navigation Keys for Routing.
+///
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+
+///
+/// Highest level widget. Implements routing.
+///
+class Root extends StatelessWidget {
+  Root({super.key});
+
+  final GoRouter _router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/home',
+    routes: [
+      // Shell creates a shell, where a screen will be placed into.
+      // Even when the screens are changed (routing). Shell
+      // remains in place!
+      // StatefulShellRoute creates a seperate navigator for each screen.
+      // Which is great when you want to preserve seperate navigation stacks
+      // between screens.
+      // I believe the indexedStack constuctor uses an indexed stack to maintain the
+      // state of widgets.
+      StatefulShellRoute.indexedStack(
+        builder: (BuildContext context, GoRouterState state,
+            StatefulNavigationShell navigationShell) {
+          return ScaffoldWithNavBar(navigationShell: navigationShell);
+        },
+        branches: [
+          // define branch and routes within branch (you can have routes in routes). Each branch has a navigator.
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/home',
+                builder: (BuildContext context, GoRouterState state) =>
+                    const HomePage(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            // It's not necessary to provide a navigatorKey if it isn't also
+            // needed elsewhere. If not provided, a default key will be used.
+            routes: [
+              GoRoute(
+                  path: '/settings',
+                  builder: (BuildContext context, GoRouterState state) =>
+                      const Center(child: Text('settings'))),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'FlutterBlueprint',
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        slivers: <Widget>[
+          const SliverAppBar(
+            stretch: true,
+            expandedHeight: 160,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text('Flight Report'),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              const <Widget>[
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Sunday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Monday'),
+                  subtitle: Text('sunny, h: 80, l: 65'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/util/scaffold_with_navbar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+// Builds the "shell" for the app by building a Scaffold with a
+/// BottomNavigationBar, where [child] is placed in the body of the Scaffold.
+class ScaffoldWithNavBar extends StatelessWidget {
+  /// Constructs an [ScaffoldWithNavBar].
+  const ScaffoldWithNavBar({
+    required this.navigationShell,
+    Key? key,
+  }) : super(key: key ?? const ValueKey<String>('ScaffoldWithNavBar'));
+
+  /// The navigation shell and container for the branch Navigators.
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navigationShell.currentIndex,
+        destinations: const [
+          NavigationDestination(
+            label: 'Section A',
+            icon: Icon(Icons.home),
+            tooltip: '',
+          ),
+          NavigationDestination(
+            label: 'Section B',
+            icon: Icon(Icons.settings),
+            tooltip: '',
+          ),
+        ],
+        onDestinationSelected: _goBranch,
+      ),
+    );
+  }
+
+  /// Navigate to the current location of the branch at the provided index when
+  /// tapping an item in the BottomNavigationBar.
+  void _goBranch(int index) {
+    // When navigating to a new branch, it's recommended to use the goBranch
+    // method, as doing so makes sure the last navigation state of the
+    // Navigator for the branch is restored.
+    navigationShell.goBranch(
+      index,
+      // A common pattern when using bottom navigation bars is to support
+      // navigating to the initial location when tapping the item that is
+      // already active. This example demonstrates how to support this behavior,
+      // using the initialLocation parameter of goBranch.
+      initialLocation: index == navigationShell.currentIndex,
+    );
+  }
+}
